e2f7cb05eeb36b04829ff4ea1f74455364300a4a
Frosendal <46622045+Frosendal@users.noreply.github.com>
2020-04-11 11:42:45 +0200

    Added Restaurant controller and html. Need database to develop further
    Tried to configure Microsoft Identity, but need db to do se.

--- Morgenmadsbuffeten/Morgenmadsbuffeten/Startup.cs
+++ Morgenmadsbuffeten/Morgenmadsbuffeten/Startup.cs
@@ -34,6 +34,7 @@ namespace Morgenmadsbuffeten
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
